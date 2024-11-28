dafc3efa4745ed2b8fb0efd0fc9360855a3c48d7
joaopbueno <mrjoao16@gmail.com>
2024-11-28 14:56:14 -0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by João Bueno on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
